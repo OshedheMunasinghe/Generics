5ead58d2efeb0a6f3527f33b9af5123ff1f226a5
OshedheMunasinghe <oshedhe91@gmail.com>
2020-03-28 21:57:53 +0100

    typecast

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Milk milk = new Milk();
+        Oranges oranges = new Oranges();
+
+        Box boxOfMilk = new Box();
+        Box boxOfOranges = new Box();
+
+        boxOfMilk.add(milk);
+        boxOfOranges.add(oranges);
+
+        // boxOfMilk.remove().drin??  //how can you call to drink first and then call remove?
+
+        //the answer is you need to typecast
+        ((Milk)boxOfMilk.remove()).drink(); //typcast the milk then wrap around to remove.. otherwise you can't call drink()
+        ((Oranges)boxOfOranges.remove()).juggle();
+
+
+
     }
 }
